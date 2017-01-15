65e1e0d5dcab00da8003d2b9eda8a231c4a69245
Ed <ed.walker@hotmail.co.uk>
2017-01-15 18:58:25 +0000

    angular carousel working

--- js/controller.js
+++ js/controller.js
@@ -1,68 +1,28 @@
 angular.module('RouteControllers', [])
     .controller('HomeController', function($scope) {
-        $scope.title = "Welcome!";
+      $scope.myInterval = 5000;
+      $scope.noWrapSlides = false;
+      $scope.active = 0;
+  
+      // My slides
+      $scope.slides = [
+        {
+          image: "img/bobmarley1.jpg",
+          // text: "a",
+          id: 0
+        },
+        {
+          image: "img/jimmycliff5.jpg",
+          // text: "b",
+          id: 1
+        },
+        {
+          image: "img/reggae3.jpg",
+          // text: "c",
+          id: 2
+        },
+      ];
     })
     .controller('BandController', function($scope){
     	$scope.title = "Band Page";
     });
-
-
-angular.module('ui.bootstrap', [])
-	.controller('CarouselDemoCtrl', function ($scope) {
-  		$scope.myInterval = 5000;
-  		$scope.noWrapSlides = false;
-  		$scope.active = 0;
-  		var slides = $scope.slides = [];
-  		var currIndex = 0;
-
-  		$scope.addSlide = function() {
-    		var newWidth = 600 + slides.length + 1;
-    		slides.push({
-      		image: '//unsplash.it/' + newWidth + '/300',
-      		text: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 4],
-      		id: currIndex++
-    		});
-  		};
-
-  		$scope.randomize = function() {
-    		var indexes = generateIndexesArray();
-    		assignNewIndexesToSlides(indexes);
-  		};
-
-  		for (var i = 0; i < 4; i++) {
-    		$scope.addSlide();
-  		}
-
-  		// Randomize logic below
-
-  		function assignNewIndexesToSlides(indexes) {
-    		for (var i = 0, l = slides.length; i < l; i++) {
-      		slides[i].id = indexes.pop();
-    		}
-  		}
-
-  		function generateIndexesArray() {
-    		var indexes = [];
-    		for (var i = 0; i < currIndex; ++i) {
-      		indexes[i] = i;
-    		}
-    		return shuffle(indexes);
-  		}
-
-  		// http://stackoverflow.com/questions/962802#962890
-  		function shuffle(array) {
-    		var tmp, current, top = array.length;
-
-    		if (top) {
-      			while (--top) {
-	        		current = Math.floor(Math.random() * (top + 1));
-	        		tmp = array[current];
-	        		array[current] = array[top];
-	        		array[top] = tmp;
-      			}
-    		}
-
-    		return array;
-  		}
-	});
-	
